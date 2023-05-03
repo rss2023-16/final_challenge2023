c9848760c7480cde0d116194c9b098bec26f8e23
lablab987 <lasyabalachandran@gmail.com>
2023-05-03 20:44:18 +0000

    updtaes

--- city_driving/src/stop_sign.py
+++ city_driving/src/stop_sign.py
@@ -5,12 +5,14 @@ import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
 from stop_detector.stop_detector import SignDetector
 
+from final_challenge2023.msg import ConeLocation, ConeLocationPixel
+
 
 class StopSign():
      def __init__(self):
-        rospy.Subscriber("/relative_cone", SignDetector, self.stop_detection_callback)
+        rospy.Subscriber("/stop_sign", ConeLocation, self.stop_detection_callback)
         DRIVE_TOPIC = rospy.get_param("~drive_topic") # set in launch file; different for simulator vs racecar
         self.drive_pub = rospy.Publisher(DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
 
-    def stop_detection_callback():
-        
+    def stop_detection_callback(self, pos):
+        pass
